bff0d6942f0147d0ff591400737987e5c4a290a1
Vitalix7 <86667434+Floppy-3@users.noreply.github.com>
2023-11-16 00:25:26 +0300

    Add files via upload

--- settingsPacket.py
+++ settingsPacket.py
@@ -1,6 +1,18 @@
-x=0
-v=0
-settings3=0
+from PIL import Image, ImageDraw
+import random
+
+files = {
+    'settingsfilename': "settings.json",
+    'backupfilename': 'Settingsbackup.properties',
+    'imagefilename': 'pack.png'
+}
+sfn = files['settingsfilename']
+bfn = files['backupfilename']
+imgfn = files['imagefilename']
+
+x = 0
+v = 0
+settings3 = 0
 
 class settings:
     def help():
@@ -15,36 +27,49 @@ class settings:
             print('Не реалізовано!!!')
         elif chs == 'backup':
             print('Виконує копіювання налаштувань з резервного файлу')
-    #def load():
+
+    # def load():
     #    set = open("E:\PyProjectsFiles\PixelMixer\Settings.json", 'r')
     #    setting = list(set.read)
     #    print(setting)
 
     def print():
-        global x
-        set = open("Settings.json", 'r')
+        global x, sfn
+        set = open(sfn, 'r')
         settings = set.read()
         while x < len(settings):
             print(settings[x])
-            x=+1
+            x = +1
             pass
 
-    def backup(filename,mode):
-        set = open("Settings.json", 'r+')
-        backup = open(filename,mode)
+    def backup(filename,backupfilename, mode):
+        set = open(filename, 'r+')
+        backup = open(backupfilename, mode)
         set.write(backup.read())
         print("Backup complete")
 
+    # def close():
+    #     set.close
 
-    def close():
-        set.close
+    # def clearing(divider):
+    #     global v
+    #     global settings3
+    #     set = open("Settings.json", 'r+')
+    #     settings = set.read()
+    #     while v <= int('2'):
+    #         settings3 = settings.split(divider)
+    #         settings3.pop(settings3.index(divider))
+    #     return settings3
 
-    def clearing(divider):
-        global v
-        global settings3
-        set = open("Settings.json", 'r+')
-        settings = set.read()
-        while v <= int('2'):
-            settings3 = settings.split(divider)
-            settings3.pop(settings3.index(divider))
-        return settings3
+
+class img_edit:
+    def eclipse(cordxy, maxeclipse, mineclipse, filename=imgfn):
+        img = Image.open(filename)
+        pixel = img.getpixel(cordxy)
+        red = random.randint(pixel[0] - mineclipse, pixel[0] + maxeclipse)
+        blue = random.randint(pixel[1] - mineclipse, pixel[1] + maxeclipse)
+        green = random.randint(pixel[2] - mineclipse, pixel[2] + maxeclipse)
+        alpha = pixel[3]
+        colors = (red, blue, green, alpha)
+        print(colors)
+        return colors
